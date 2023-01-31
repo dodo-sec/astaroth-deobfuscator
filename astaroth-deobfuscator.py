249c8935522ebe93168857e80e955e0874a70b59
dodo-sec <110191752+dodo-sec@users.noreply.github.com>
2023-01-31 13:47:30 +0000

    Code revamp
    Now does a single hide for a range of sequential blocks of arguments + call to padding function, instead of doing it block by block

--- astaroth-deobfuscator.py
+++ astaroth-deobfuscator.py
@@ -1,19 +1,28 @@
+import ida_bytes
 import idautils
 import idc
-import ida_bytes
 
 start = 0
 end = 0
+time_waster_slide = False
+
+#Remember to replace 'sub_CHANGEME' with the name of your function
 
 for function in idautils.Functions():
     #Loop on each instruction of the function
     for x in idautils.FuncItems(function):
-        #Find the call to the junk function
-        if idc.print_insn_mnem(x) == 'call' and idc.print_operand(x, 0) == 'time_waster_3000':
-            #Go backwards to the push of the first argument
+        #Find the useless padding function
+        if idc.print_insn_mnem(x) == 'call' and idc.print_operand(x, 0) == 'sub_CHANGEME':
+            #Mark start address
             start = x - 35
-            #Go forwards to include the call instruction (5 bytes long) to the hidden block
-            end = x + 5
-            #Hide it
-            ida_bytes.del_hidden_range(start)
-            ida_bytes.add_hidden_range(start,end,'','','',0xFFFFFF)
+            time_waster_slide = True
+            #Loop until finding the end of a block of calls to padding function
+            while time_waster_slide:
+                if idc.print_insn_mnem(x + 40) != 'call' and idc.print_operand(x + 40, 0) != 'sub_CHANGEME':
+                    end = x + 5
+                    #Hide range
+                    ida_bytes.add_hidden_range(start,end,'','','',0xFFFFFF)
+                    time_waster_slide = False
+                else:
+                    x += 40
+            
